995e3267a9f287ccd7df1d0865bf52105648870c
Iryna <iryna.sherepot@gmail.com>
2019-02-20 15:31:58 -0800

    Create asyncIO.js

---
+++ asyncIO.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.readFile(process.argv[2], function(err, text){
+
+  var textStr = text.toString();
+  var lines = textStr.split('\n');
+
+  console.log(lines.length-1);
+});
