2700bcaef3066a11f7678b5aa81a93ecb54bec72
Iryna <iryna.sherepot@gmail.com>
2019-02-20 12:14:45 -0800

    Updated #2Baby Steps

--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-console.log("HELLO WORLD"); 
+//program that accepts numbers as command-line arguments and prints
+//the sum of numbers to the console
+
+var sum = 0;
+for(var i =2; i<process.argv.length; i++){
+  sum += Number(process.argv[i]);
+}
+console.log(sum);
+return sum;
