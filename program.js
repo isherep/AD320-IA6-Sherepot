6dc3232c82778b3c734166f6bbdd8ee8d4cc1347
Iryna <iryna.sherepot@gmail.com>
2019-02-20 15:06:01 -0800

    Added #3 My First I/O

--- program.js
+++ program.js
@@ -1,9 +1,12 @@
-//program that accepts numbers as command-line arguments and prints
-//the sum of numbers to the console
+// Write a program that uses a single synchronous filesystem operation to
+// read a file and print the number of newlines (\n) it contains to the
+// console (stdout)
 
-var sum = 0;
-for(var i =2; i<process.argv.length; i++){
-  sum += Number(process.argv[i]);
-}
-console.log(sum);
-return sum;
+var fs = require('fs');
+
+var lines = fs.readFileSync(process.argv[2]);//returns buffer object
+var allLines = lines.toString();// converrt buffer to string
+ var splitLines = allLines.split('\n');
+console.log(splitLines.length -1);
+//return strLines;
+//return strLines.length-1;
